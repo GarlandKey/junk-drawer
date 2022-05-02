8bb0b7f5a1c535b10593bc0ceaf6271bf2975a09
Garland Key <garlandkey@protonmail.com>
2022-05-02 13:18:57 -0500

    refactored api fetch and adjusted css for image overlap.

--- 2022-05-03-pokedex/js/main.js
+++ 2022-05-03-pokedex/js/main.js
@@ -1,29 +1,26 @@
-/* 
- * 
- */
-document.querySelector('button').addEventListener('click', logIt)
-// document.querySelector('button').addEventListener('click', getFetch)
+document.querySelector('#pokemonInputBtn').addEventListener('click', getFetch)
 
-function logIt() {
-  const pokemon = document.querySelector('#pokemonInput').value
-  console.log(`user typed: '${pokemon}`)
-}
-
-
-/*function getFetch(){
-  const pokemon = document.querySelector('#pokemon-input').value
-  console.log(pokemon)
+function getFetch(){
+  const pokemon = document.querySelector('#pokemonInput').value.toLowerCase()
   const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+  
+  console.log(`user typed: '${pokemon}'`) // DEBUGING -- REMOVE
 
   fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        console.log(data)
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
-}
-*/
-/*`${pokemon}is a ${data.types.0.type.name} pokemon. Average height is ${data.height} foot and avegate weight is ${data.weight} pounds.`*/
+    .then(res => res.json())
+    .then(data => {
+      console.log(data) // DEBUGING -- REMOVE
+      const pokeName = `../img/pokemon-by-id/${data.id}.webp`
+      const pokeBlurb = `${pokemon} is a ${data.types[0].type.name} type pokemon. ${pokemon}'s average height is ${data.height} foot and their average weight is ${data.weight} pounds.`
 
+      document.querySelector('#pokedexPokemonImage').src = pokeName`../img/pokemon-by-id/${data.id}.webp`
+
+
+      console.log(`image location: ${pokemonImage}`) // DEBUGING -- REMOVE
+      console.log(`blurb: ${pokemonBlurb}`)
+
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    });
+}
