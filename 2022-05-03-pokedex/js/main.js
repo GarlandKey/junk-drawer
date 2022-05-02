417954c3fe0df0330df314b27985c2e20eeb760b
Garland Key <garlandkey@protonmail.com>
2022-05-02 06:17:48 -0500

    working on js bug. commiting improvements to logging

--- 2022-05-03-pokedex/js/main.js
+++ 2022-05-03-pokedex/js/main.js
@@ -1,69 +1,29 @@
 /* 
  * 
  */
-
-
-
+document.querySelector('button').addEventListener('click', logIt)
 // document.querySelector('button').addEventListener('click', getFetch)
-document.querySelector('#pokemon-name-container').addEventListener('submit', getFetch);
+
+function logIt() {
+  const pokemon = document.querySelector('#pokemonInput').value
+  console.log(`user typed: '${pokemon}`)
+}
 
 
-function getFetch(){
-  const pokemon = document.querySelector('#pokemon-name').value
+/*function getFetch(){
+  const pokemon = document.querySelector('#pokemon-input').value
+  console.log(pokemon)
   const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-
         console.log(data)
-
-          pokeStore.push(data.types[0].type.name)
-          pokeImg.push(data.sprites.front_shiny)
-        
-        fetch(url2)
-        .then(res => res.json()) // parse response as JSON
-        .then(data => {
-      
-          if((pokeStore[0] === "grass" && pokeStore[1] === 'water')){
-            document.querySelector('#pokeImg1').src = pokeImg[0]
-            document.querySelector('#pokeImg2').src = pokeImg[1]
-            document.querySelector('h2').innerText = " 2x > "
-          }
-        })
-        .catch(err => {
-            console.log(`error ${err}`)
-        });
-
-
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
 }
-
-/*
-type =[
-  'bug',
-  'dark',
-  'dragon',
-  'electric',
-  'fairy',
-  'fighting',
-  'fire',
-  'flying',
-  'ghost',
-  'grass',
-  'ground',
-  'ice',
-  'normal',
-  'poison',
-  'psychic',
-  'rock',
-  'steel',
-  'water'
-]
-
-`${pokemon}is a ${data.types.0.type.name} pokemon. Average height is ${data.height} foot and avegate weight is ${data.weight} pounds.`
-
 */
+/*`${pokemon}is a ${data.types.0.type.name} pokemon. Average height is ${data.height} foot and avegate weight is ${data.weight} pounds.`*/
+
