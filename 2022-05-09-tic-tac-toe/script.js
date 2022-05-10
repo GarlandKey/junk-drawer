ee9d0e7cb7a86026cc4f0db02303484a55d2a9dd
Garland Key <garlandkey@protonmail.com>
2022-05-10 14:52:27 -0500

    group project final update

--- 2022-05-09-tic-tac-toe/script.js
+++ 2022-05-09-tic-tac-toe/script.js
@@ -1,7 +1,6 @@
-/* EVERYTHING WORKS, BUT THERE IS A BUG NOTED ON LINE 67 */
+/* DO NOT CHANGE -- THIS ALL WORKS */
 
 /* global variables */
-const spaces = Array.from(document.querySelectorAll(".space"))
 const x = 'x'
 const o = 'o'
 let turn = x
@@ -9,6 +8,7 @@ let turnCount = 0
 
 /* query selectors */
 const btn = document.querySelector('button')
+const spaces = Array.from(document.querySelectorAll(".space"))
 const a1 = document.querySelector('#a1')
 const a2 = document.querySelector('#a2')
 const a3 = document.querySelector('#a3')
@@ -20,86 +20,103 @@ const c2 = document.querySelector('#c2')
 const c3 = document.querySelector('#c3')
 const gameStatus = document.querySelector('#gameStatus')
 
-
 /* event listeners */
 btn.addEventListener('click', start)
-
 
 /* START */
 start()
 
 /* Initiates new game */
 function start() {
-    clearBoard()
-    turn = x
-    spaces.forEach(e => e.addEventListener("click", playTurn))
-}
+  clearBoard()
+  turnCount = 0
+  turn = x
+  gameStatus.innerText = `Turn: ${x}`
+  spaces.forEach(e => e.addEventListener("click", playTurn))
+  spaces.forEach(e => e.style.color = "black")
 
+  // Displays button click temporarily
+  btn.style.borderColor = "gray"
+  setTimeout(() => { btn.style.borderColor = "black" }, 250)
+}
 
 /* Go through each space element and clear them. */
 function clearBoard() {
-    spaces.forEach(e => e.innerText = "")
-    gameStatus.innerText = ''
+  spaces.forEach(e => e.innerText = "")
+  gameStatus.innerText = ''
 }
 
 /* Turn logic */
 function playTurn(e) {
-    e.preventDefault()
-    e.target.innerText = turn
-    e.target.removeEventListener("click", playTurn)
-    swapTurn()
-    turnCount++
-    checkWin()
+  e.preventDefault()
+  e.target.innerText = turn // places a symbol on the board
+  e.target.removeEventListener("click", playTurn) // removes event listener so space can't be played more than once
+  turnCount++ // needed to track for a draw in checkWin()
+
+  swapTurn()
+  checkWin()
+   // keep after checkWin(), so we can return the winner as the current player
 }
 
-
+/* Swaps symbol for next turn */
 function swapTurn() {
-    if(turn === x) {
-        turn = o
-        gameStatus.innerText = `Turn: ${o}`
-    } else if(turn === o) {
-        turn = x
-        gameStatus.innerText = `Turn: ${x}`
-    }
+  if (turn === x) {
+    turn = o
+    gameStatus.innerText = `Turn: ${o}`
+  } else if (turn === o) {
+    turn = x
+    gameStatus.innerText = `Turn: ${x}`
+  }
 }
 
-/* TODO - BUG FIX - ONCE GAME IS OVER, PLAYERS CAN STILL ADD SYMBOLS TO BOARD */
 function endGame(player) {
-  if(player === 'draw') {
+  if (player === 'draw') {
     gameStatus.innerText = `Draw!`
   } else {
     gameStatus.innerText = `${player} wins!`
   }
+  spaces.forEach(e => e.removeEventListener("click", playTurn))
+  spaces.forEach(e => e.style.color = "gray")
 }
 
+/* Checks for all possible win or draw conditions */
 function checkWin() {
-  // horizontal wins
+
+  /*  
+      Thoughts? -  a1.innerText.value == a2.innerText.value 
+      && a2.innerText.value == a2.innerText.value
+
+      This would work and be DRY
+      endgame(turn)?
+  */
+
+  // vertical
   if ((a1.innerText === x && b1.innerText === x && c1.innerText === x) ||
-      (a2.innerText === x && b2.innerText === x && c2.innerText === x) ||
-      (a3.innerText === x && b3.innerText === x && c3.innerText === x)) {
-      endGame(x)
+    (a2.innerText === x && b2.innerText === x && c2.innerText === x) ||
+    (a3.innerText === x && b3.innerText === x && c3.innerText === x)) {
+    endGame(x)
   } else if ((a1.innerText === o && b1.innerText === o && c1.innerText === o) ||
-      (a2.innerText === o && b2.innerText === o && c2.innerText === o) ||
-      (a3.innerText === o && b3.innerText === o && c3.innerText === o)) {
-      endGame(o)
-  // vertical wins
+    (a2.innerText === o && b2.innerText === o && c2.innerText === o) ||
+    (a3.innerText === o && b3.innerText === o && c3.innerText === o)) {
+    endGame(o)
+    // horizontal
   } else if ((a1.innerText === x && a2.innerText === x && a3.innerText === x) ||
-      (b1.innerText === x && b2.innerText === x && b3.innerText === x) ||
-      (c1.innerText === x && c2.innerText === x && c3.innerText === x)) {
-      endGame(x)
+    (b1.innerText === x && b2.innerText === x && b3.innerText === x) ||
+    (c1.innerText === x && c2.innerText === x && c3.innerText === x)) {
+    endGame(x)
   } else if ((a1.innerText === o && a2.innerText === o && a3.innerText === o) ||
-      (b1.innerText === o && b2.innerText === o && b3.innerText === o) ||
-      (c1.innerText === o && c2.innerText === o && c3.innerText === o)) {
-      endGame(o)
-  // diagonal wins
+    (b1.innerText === o && b2.innerText === o && b3.innerText === o) ||
+    (c1.innerText === o && c2.innerText === o && c3.innerText === o)) {
+    endGame(o)
+    // diagonal wins
   } else if ((a1.innerText === x && b2.innerText === x && c3.innerText === x) ||
-      (a3.innerText === x && b2.innerText === x && c1.innerText === x)) {
-      endGame(x)     
+    (a3.innerText === x && b2.innerText === x && c1.innerText === x)) {
+    endGame(x)
   } else if ((a1.innerText === o && b2.innerText === o && c3.innerText === o) ||
-      (a3.innerText === o && b2.innerText === o && c1.innerText === o)) {
-      endGame(o)
-  // draw
+    (a3.innerText === o && b2.innerText === o && c1.innerText === o)) {
+    endGame(o)
+    // draw
   } else if (turnCount === 9) {
-    endGame('draw')  
+    endGame('draw')
   }
 }
